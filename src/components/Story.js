09bd7ab5bce79c51ed4b34674c75df0632742699
ZhangAnli <zhanganli1998@hotmail.com>
2021-02-17 16:21:06 +0800

    Complete Stories Container

---
+++ src/components/Story.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect} from 'react'
+import { getStory } from '../services/hnApi'; 
+
+export const Story = ({ storyId}) => {
+
+    const [story, setStory] = useState({}); 
+
+    useEffect(() => {
+        getStory(storyId).then(data => data && data.url && setStory(data)); 
+    }, []); 
+
+    return story && story.url ? (
+            <>
+                <a href={story.url}>
+                    <p>{story.title}</p>
+                </a>
+                By: <p>{story.title}</p>
+                Posted: <p>{story.time}</p>
+            </>
+        ) : null; 
+}
+
+export default Story; 
